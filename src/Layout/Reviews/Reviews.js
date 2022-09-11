2c191677231aafd390e2f902365e454235b3ee2f
Bradley Owens <bradowens17@hotmail.com>
2022-09-11 17:20:39 +1000

    Add component Reviews to application with skeleton for layout.

---
+++ src/Layout/Reviews/Reviews.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Reviews.module.css";
+
+const Reviews = () => {
+  return (
+    <div className="">
+      <div className="">
+        <div className="">
+          <h1>Name</h1>
+          <p>Text</p>
+        </div>
+        <div className="">
+          <h1>Name</h1>
+          <p>Text</p>
+        </div>
+        <div className="">
+          <h1>Name</h1>
+          <p>Text</p>
+        </div>
+        <div className="">
+          <h1>Name</h1>
+          <p>Text</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
