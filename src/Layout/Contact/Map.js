c52acddf7c2d49c764853353dff084ed9ba434b9
Bradley Owens <bradowens17@hotmail.com>
2022-09-13 14:05:41 +1000

    Add map using google maps API centered at the coordinates of clients location for users

--- src/Layout/Contact/Map.js
+++ src/Layout/Contact/Map.js
@@ -1,6 +1,51 @@
-import React, { Component } from "react";
-import styles from "./Map.module.css";
+import React from "react";
 
-const Map = () => {};
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
-export default Map;
+const mapStyle = {
+  width: "700px",
+  height: "400px",
+  borderRadius: "20px",
+};
+
+const center = {
+  lng: 153.066666,
+  lat: -26.95,
+};
+
+const Map = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyAxrrvwuspgkZ7eFGiZJ_AuKDnIRaZkAmI",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  // const onLoad = React.useCallback(function callback(map) {
+  //   const bounds = new window.google.maps.LatLngBounds(center);
+  //   map.fitBounds(bounds);
+  //   setMap(map);
+  // }, []);
+
+  // const onUnmount = React.useCallback(function callback(map) {
+  //   setMap(null);
+  // }, []);
+
+  const zoom = 8;
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={mapStyle}
+      center={center}
+      zoom={zoom}
+      // onLoad={onLoad}
+      // onUnmount={onUnmount}
+    >
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+};
+
+export default React.memo(Map);
